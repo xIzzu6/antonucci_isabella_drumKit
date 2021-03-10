894f854270a0fe1884ceb687d39e362ca40918ae
Izzu A <>
2021-03-10 14:31:07 -0500

    finished drum kit

--- js/main.js
+++ js/main.js
@@ -7,20 +7,33 @@
   // functions go in the middle
 
   // get the keycode so that we can match it to an audio element
-  function catchKeyCode(e) {
+  function catchKeyCode(event) {
     let key = event.keyCode
     // debugger;
 
     let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+      currentKey = document.querySelector(`div[data-key="${event.keyCode}"]`);
 
+    // if there's no matching audio element, then quit
     if (!audio) { return; }
 
+    // animate the div that corresponds to the key we pressed
+    currentKey.classList.add("playing");
+
+    audio.currentTime = 0;
     audio.play();
   }
 
+  // remove the playing class after the transition fires the first time
+  function resetKey(event) {
+    // debugger;
+    if (event.propertyName === "transform") {
+      this.classList.remove("playing");
+    }
+  }
 
   // event handling goes at the bottom
-  // keyGraphics.forEach(key => key.addEventListener("keyup", catchKeyCode));
+  keyGraphics.forEach(key => key.addEventListener("trainsitionend", resetKey));
 
   window.addEventListener("keyup", catchKeyCode);
 })();
